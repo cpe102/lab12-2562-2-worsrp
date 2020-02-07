25ef45b537abcb2650223ae25d55b96b6cad69f6
Sirapop Para <worwee07@MacBook-Pro.local>
2020-02-07 16:23:16 +0700

    edited

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -13,8 +13,22 @@ int main()
     cout << "Average = " << B[0];
     cout << "\nS.D. = " << B[1];
     cout << "\nMax = " << B[2];
-    cout << "\nMin = " << B[3];
+    cout << "\nMin = " << B[3] << endl;
     return 0;
 }
 
-//Write definition of stat() here 
+void stat(const double A[],int N,double B[]){
+    int i;
+    double mean,sd,max,min,sum=0,sump2=0;
+    for(i=0;i<N;i++){
+        sum += A[i];
+        sump2 += pow(A[i],2); 
+        if(A[i]>max||i==0) max=A[i];
+        if(A[i]<min||i==0) min=A[i];
+    }
+    mean=sum/N;
+    B[0] = mean;
+    B[1] = sqrt((1.0/N*sump2)-pow(mean,2));
+    B[2] = max;
+    B[3] = min;
+}
--- lab12_2.cpp
+++ lab12_2.cpp
@@ -15,11 +15,36 @@ int main()
     int s,x,y;
     do{
         showImage(image);
-        cout << "Input your brush size and location:";
+        cout << "\nInput your brush size and location:";
         cin >> s >> x >> y;
         updateImage(image,s,x,y);
     }while(s != 0 && x != 0 && y != 0 );
-             return 0;    
+    return 0;
 }
 
-// Write definition of updateImage() and showImage() here
+void updateImage(bool image[][M],int s,int x,int y){
+    int i,j;
+    for(i=0;i<N;i++){
+        for(j=0;j<M;j++){
+            if(image[i][j]==1) continue;
+            if(sqrt(pow((i-x),2)+pow((j-y),2))<=s-1)  image[i][j]=1;
+            else  image[i][j]=0;
+        }
+    }
+}
+
+void showImage(const bool image[][M]){
+    int i,j;
+    for(i=0;i<M+2;i++) cout << "-"; 
+    for(i=0;i<N;i++){
+        cout <<"\n"  << "|";
+        for(j=0;j<M;j++){
+            if(image[i][j]==true) cout << "*";
+            else cout << " ";
+        }
+        cout << "|";
+    }
+    cout << "\n";
+    for(i=0;i<M+2;i++) cout  << "-";
+
+}
